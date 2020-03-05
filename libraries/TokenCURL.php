b97bfd33d09b6d7b2f432d735bef450dbd4c2c4d
Francis Ilechukwu <francis94c@gmail.com>
2020-03-05 06:42:40 +0100

    Update TokenCURL.php

--- libraries/TokenCURL.php
+++ libraries/TokenCURL.php
@@ -2,20 +2,53 @@
 declare(strict_types=1);
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class TokenCURL {
-
+class TokenCURL
+{
+/**
+ * [GET description]
+ * @var string
+ */
   const GET  = 'GET';
+
+  /**
+   * [POST description]
+   * @var string
+   */
   const POST = 'POST';
 
+  /**
+   * [private description]
+   * @var [type]
+   */
   private $method;
+
+  /**
+   * [private description]
+   * @var [type]
+   */
   private $userAgent;
 
-  function __construct(string $method, string $userAgent='Token Client v0.0.1') {
+  /**
+   * [__construct description]
+   * @date  2020-03-05
+   * @param string     $method    [description]
+   * @param string     $userAgent [description]
+   */
+  function __construct(string $method, string $userAgent='Token Client v0.0.1')
+  {
     $this->method = $method;
     $this->userAgent = $userAgent;
   }
 
-  function __invoke(string $url, array $body=null):array {
+  /**
+   * [__invoke description]
+   * @date   2020-03-05
+   * @param  string     $url  [description]
+   * @param  [type]     $body [description]
+   * @return [type]           [description]
+   */
+  function __invoke(string $url, array $body=null):array
+  {
     if ($body != null) $body = http_build_query($body);
     $ch = curl_init($url);
 
@@ -25,20 +58,21 @@ class TokenCURL {
     if (ENVIRONMENT == 'development') {
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     }
+
     // Header.
     $header = [
       'Content-Type: application/x-www-form-urlencoded'
     ];
+
     if ($body != null) $header[] = 'Content-Length: '.strlen($body);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
     curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
     // Request Method and Body.
-    if ($this->method == self::POST) {
-      curl_setopt($ch, CURLOPT_POST, true);
-      if ($body != null) {
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-      }
+    if ($body != null) {
+      //curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
     }
+
     // Exec.
     $response = curl_exec($ch);
     $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
